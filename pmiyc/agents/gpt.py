4145e0f6d11d9099472c0f8773160959fd574145
Nimet Beyza Bozdag <68226953+beyzabozdag@users.noreply.github.com>
2025-02-15 14:33:09 -0600

    Update gpt.py

--- pmiyc/agents/gpt.py
+++ pmiyc/agents/gpt.py
@@ -28,9 +28,9 @@ class ChatGPTAgent(Agent):
             else seed
         )
         self.client = AzureOpenAI( 
-            azure_endpoint = "https://uiuc-convai.openai.azure.com/",
+            azure_endpoint = "endpoint",
             api_key=os.getenv("AZURE_OPENAI_KEY"),
-            api_version="2024-02-15-preview"
+            api_version="version"
         )
         self.temperature = temperature
         self.max_tokens = max_tokens
